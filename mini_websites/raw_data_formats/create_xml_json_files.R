640e6dbe39d11aa85fe7a46472eef548f34caccf
Your Name <jansodoge@web.dem>
2021-03-08 22:52:07 +0100

    Update create_xml_json_files.R

--- mini_websites/raw_data_formats/create_xml_json_files.R
+++ mini_websites/raw_data_formats/create_xml_json_files.R
@@ -2,6 +2,8 @@
 
 library(readxl)
 library(tidyverse)
+library(rjson)
+library(kulife)
 tour_de_france <- read_excel("sicss_scraping_school/mini_websites/raw_data_formats/tour_de_france.xlsx")
 
 
@@ -9,3 +11,10 @@ tour_de_france <- tour_de_france %>%
   select(Year, 'Total distance (km)', 'Number of stages', Entrants, 
          Finishers, Winner, "Winner's Team", "Winner's Nationality"   )
 
+json_tour_de_france <- toJSON(unname(split(tour_de_france, 1:nrow(tour_de_france))))
+
+write(json_tour_de_france, "tour_de_france.json")
+
+
+#to write XML file
+write.xml(tour_de_france, file= "tour_de_france.xml")
